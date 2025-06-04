c2451d7aa89c6676aa91d0c5d4101db02512e237
ernesto mellado vallecillo <ernestomelladovallecillo@gmail.com>
2025-06-03 20:33:30 -0700

    adding handleToggle

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ function App() {
 const task = useTaskStore((state)=>state);
 
 
-const [arrayTask, setArrayTask] = useState([{id:125, title:"primera tarea", taskStatus:true}]);
+const [arrayTask, setArrayTask] = useState([{id:125, title:"primera tarea", taskStatus:false}]);
 
 const addTask = (value:number)=>{
-    setArrayTask([...arrayTask, {id:value, title:"nueva tarea"}]);
+    setArrayTask([...arrayTask, {id:value, title:"nueva tarea", taskStatus:false}]);
 }
 
 const deleteTask = (value:number)=>{
@@ -18,9 +18,19 @@ const deleteTask = (value:number)=>{
     setArrayTask(nuevoArray);
 }
 
-const toggleTask = (value) => {
-  
+const toggleTask = (value:number) => {
+  console.log("imprimir value con el click", value)
+ 
 
+  const pickTask = arrayTask.filter((array) => array.id === value);
+ 
+  if(pickTask.length > 1){
+    let resultToogleTask = arrayTask.map((task) =>{
+      task.id === value ? {...task, taskStatus: !task.taskStatus}: task
+    })  
+    console.log("resultToogleTask", resultToogleTask)
+  }
+  console.log(pickTask);
 }
 
 console.log(arrayTask);
@@ -34,7 +44,7 @@ console.log(arrayTask);
         <ul>
           {arrayTask.map(arrayTask =>(
             <li key={arrayTask.id} >
-              <p><input onChange={()=>{toggleTask(arrayTask.id)}} type='checkbox'/ >{arrayTask.title}<button onClick={()=>deleteTask(arrayTask.id)}>Eliminar</button></p>
+              <p><input onChange={()=>{toggleTask(arrayTask.id)}} type='checkbox'/ ><span className={arrayTask.taskStatus ? 'line-through text-gray-400' : ''}>{arrayTask.title}</span><button onClick={()=>deleteTask(arrayTask.id)}>Eliminar</button></p>
             </li>
           ))}
         </ul>
