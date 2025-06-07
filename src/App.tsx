51919aa323580bce8d5eac9b17e664e9b1dcb455
ernesto mellado vallecillo <ernestomelladovallecillo@gmail.com>
2025-06-07 12:43:03 -0700

    adding zustand add task and delete task from zustand

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
 import './App.css'
 import {useTaskStore} from "./store/useTaskStore"
-import { useState } from 'react';
+
 
 function App() {
   
-const task = useTaskStore((state)=>state);
-const addTaskZustand = useTaskStore((state) => state.addTask)
+const arrayTaskZustand = useTaskStore((state)=>state.arrayTask);
+const addTaskZustand = useTaskStore((state) => state.addTask);
 
 
 
-const [arrayTask, setArrayTask] = useState([{id:125, title:"primera tarea", taskStatus:false}]);
-
-const addTask = (value:number)=>{
-    setArrayTask([...arrayTask, {id:value, title:"nueva tarea", taskStatus:false}]);
-}
-
+/*
 const deleteTask = (value:number)=>{
     const nuevoArray = arrayTask.filter((arrayTask) => arrayTask.id !== value);
     setArrayTask(nuevoArray);
 }
 
 const toggleTask = (value:number) => {
-  addTaskZustand(28);
-  console.log("imprimir el nuevo valor de task", task)
- 
-
 
   const pickTask = arrayTask.filter((array) => array.id === value);
  
@@ -37,7 +28,7 @@ const toggleTask = (value:number) => {
   }
   console.log("pickTask", pickTask);
 }
-
+*/
   return (
     <>
     <div>
@@ -46,14 +37,14 @@ const toggleTask = (value:number) => {
       </div>
       <div>
         <ul>
-          {arrayTask.map(arrayTask =>(
+          {arrayTaskZustand.map(arrayTask =>(
             <li key={arrayTask.id} >
               <p><input onChange={()=>{toggleTask(arrayTask.id)}} type='checkbox'/ ><span className={arrayTask.taskStatus ? 'line-through text-gray-400' : ''}>{arrayTask.title}</span><button onClick={()=>deleteTask(arrayTask.id)}>Eliminar</button></p>
             </li>
           ))}
         </ul>
         <div>
-          <button onClick={()=>addTask(arrayTask.length)}>agregar nueva tarea</button>
+          <button onClick={()=>addTaskZustand(arrayTaskZustand.length)}>agregar nueva tarea</button>
         </div>
       </div>
     </div>
--- src/store/useTaskStore.ts
+++ src/store/useTaskStore.ts
@@ -1,17 +1,27 @@
 import { create } from 'zustand'
 
 interface Task {
+  deleteTask: any
   addTask: any
-  arrayTask : {
+  arrayTask : [{
     id:number, 
     title: string
     taskStatus: boolean
-  }
+  }]
 }
 
 export const useTaskStore = create<Task>((set) => ({
-  arrayTask: {id:125, title:"primera tarea", taskStatus:false},
-  addTask:(value:number) => set(state => ({ arrayTask: {...state.arrayTask , id:value, title:"segunda", taskStatus:false} }))
+  arrayTask: [{id:125, title:"primera tarea", taskStatus:false}],
+  deleteTask: (value:numer) =>  {
+      const newArray= arrayTask.filter((arrayTask) => arrayTask.id !== value);
+      set((state)=>({
+        arrayTask:[newArray]
+      })
+  }
+  addTask:(value:number) => set((state) => ({ 
+    arrayTask: [...state.arrayTask, {id:value, title:"nuevo array", taskStatus:false}]
+  }))
+  
   
 
 }))
